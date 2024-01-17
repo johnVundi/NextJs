26dd74390f6c8c4c35cd9f427b545a0378ba85ea
John Vundi <johnsonvundy2@gmail.com>
2024-01-17 09:13:53 +0300

    styled the Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,9 +2,9 @@ import Link from "next/link"
 
 export default function Navbar(){
     return(
-        <nav>
-            <Link href={"/"}>Family Album</Link>
-            <Link href={"/addPhoto"}>Add Photo </Link>
+        <nav className="flex justify-between items-center bg-slate-800 px-8 py-3">
+            <Link className="text-white font-bold" href={"/"}>Family Album</Link>
+            <Link className="p-2 bg-white" href={"/addPhoto"}>Add Photo </Link>
         </nav>
     )
 }
